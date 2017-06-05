2f61f732f83b80db6cd8316da637144962aaa984
Gonzo <dggsax@gmail.com>
2017-06-05 12:30:30 -0400

    added static directory

---
+++ static/js/index.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+	// $( document ).ready( function() {
+	// 	// addSlider();
+	//     console.log( "Lets do this!" );
+	// });
+
+
+	// function addSlider() {
+	// 	$('#addSlider').click(function(){
+	// 		console.log("Clicked!");
+
+	// 	});
+	// }
+	console.log( "Lets do this!" );
+	
+	var addSlider = document.getElementById('addSlider');
+
+	addSlider.onclick = function(){
+
+		console.log( "Lets do thiiisssss" );
+	    // var script = document.createElement("script");
+	    // script.type = "text/javascript";
+	    // script.src = "Public/Scripts/filename.js."; 
+	    // document.getElementsByTagName("head")[0].appendChild(script);
+	    // return false;
+
+	}
+}
