574a1f1499bc204b127e32011ad85ed3d31c97fc
Gonzo <dggsax@gmail.com>
2017-06-06 19:08:18 -0400

    A lot

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,17 @@
+//###############
+//##           ##
+//##    WOO!   ##
+//##           ##
+//###############
+
 window.onload = function(){
 	console.log( "Lets do this!" );
 
 	var submitSliderButton = document.getElementById('submitSlider');
 	var submitTimeplotButton = document.getElementById('submitTimeplot');
 	var table = [];
-	var counter = 0;
+	var counter_a = 0;
+	var counter_b = 0;
 
 	submitSliderButton.onclick = function() {
 		submitSlider();
@@ -18,18 +25,18 @@ window.onload = function(){
 
 	// Han$dles submissions for Sliders
 	function submitSlider(){
-		counter += 1;
+		counter_a += 1;
 		// Build associative array
-		var slider = {};  // denotes an Object is being created
+		var slider = {};
 		slider.name = document.getElementById('sliderName').value;
 		slider.lowrange = document.getElementById('sliderLowrange').value;
 		slider.highrange = document.getElementById('sliderHighrange').value;
 		slider.stepsize = document.getElementById('sliderStepsize').value;
-		slider.alternate = '<input type="checkbox" class="pure-checkbox" name="slider-checkbox-'+counter+'" id="alt_'+counter+'">';
-		slider.period = '<input id="slider_period_'+counter+'" type="number" placeholder="any number" disabled>';
+		slider.alternate = '<input type="checkbox" class="pure-checkbox" name="slider-checkbox-'+counter_a+'" id="alt_'+counter_a+'">';
+		slider.period = '<input id="slider_period_'+counter_a+'" type="number" placeholder="any number" step="any" disabled>';
 		
 		// Update the table	
-		sliderTable(slider, counter);
+		sliderTable(slider, counter_a);
 
 		// Clear the form
 		clearForm('#slider');
@@ -37,6 +44,7 @@ window.onload = function(){
 
 	// Handles submissions for Timeplots
 	function submitTimeplot(){
+		counter_b += 1;
 		// Build associative array
 		var timeplot = {};  // denotes an Object is being created
 		timeplot.name = document.getElementById('timeplotName').value;
@@ -44,13 +52,8 @@ window.onload = function(){
 		timeplot.lowrange = document.getElementById('timeplotLowrange').value;
 		timeplot.highrange = document.getElementById('timeplotHighrange').value;
 
-		// Prints info to console (honestly just comment out when you know the array works...)
-		// console.log(timeplot.length);  // results in undefined
-		// console.log("Name is: " + timeplot['name']);
-		// console.log("format is: " + timeplot['format']);
-		// console.log("lowrange is: " + timeplot['lowrange']);
-		// console.log("highrange is: " + timeplot['highrange']);
-
+		// Build the table
+		timeplotTable(timeplot, counter_b);
 		// Clear the form
 		clearForm('#timeplot');
 	};
@@ -67,17 +70,18 @@ window.onload = function(){
 	// Update any table whenever a new array is submitted
 	function sliderTable(array, counter) {
 		// Grab existing/new table element
-		var updated_table = document.getElementById("slider-table");
+		var table_div = document.getElementById("slider-table-div");
 
 		// For initialization of the table, otherwise it adds column labels
 		// everytime a new thing is added...
 		if (counter < 2) {
-			
-			$(updated_table).addClass("pure-table pure-table-bordered");
-
+			var table_header = document.createElement("table");
+			$(table_header).addClass("pure-table pure-table-bordered");
+			$(table_header).attr('id','slider-table')
+			$(table_div).append(table_header);
 			// Build the table head (Columns...)
 			var updated_table_head = document.createElement("thead");
-			var thead = "<tr>";
+			var thead = "<tr><Order>";
 			for (var key in array) {
 				// do something with key
 				thead += "<th>"+key+"</th>";
@@ -85,7 +89,7 @@ window.onload = function(){
 			thead +="</tr>";
 
 			$(updated_table_head).append(thead);
-			$(updated_table).append(updated_table_head);
+			$(table_header).append(updated_table_head);
 
 			var updated_table_body = document.createElement("tbody");
 			$(updated_table_body).attr('id', 'slider-table-body')
@@ -101,20 +105,107 @@ window.onload = function(){
 		}
 		tbody += "</tr>"
 		$(updated_table_body).append(tbody);
-		$(updated_table).append(updated_table_body);
+		$(table_header).append(updated_table_body);
 	}
-	
+	function timeplotTable(array, counter) {
+		// Grab existing/new table element
+		var table_div = document.getElementById("timeplot-table-div");
+
+		// For initialization of the table, otherwise it adds column labels
+		// everytime a new thing is added...
+		if (counter < 2) {
+			var table_header = document.createElement("table");
+			$(table_header).addClass("pure-table pure-table-bordered");
+			$(table_header).attr('id','timeplot-table')
+			$(table_div).append(table_header);
+
+			// Build the table head (Columns...)
+			var updated_table_head = document.createElement("thead");
+			// <tr><th>Priority</th>
+			var thead = "<tr><th><Priority></th>";
+			for (var key in array) {
+				// do something with key
+				thead += "<th>"+key+"</th>";
+			}
+			thead +="</tr>";
+
+			$(updated_table_head).append(thead);
+			$(table_header).append(updated_table_head);
+
+			var updated_table_body = document.createElement("tbody");
+			$(updated_table_body).attr('id', 'timeplot-table-body')
+		} else {
+			var updated_table_body = document.getElementById("timeplot-table-body");
+		}
+
+		// Add a new row to the table.
+		var tbody = "<tr><td class='priority'>"+counter+"</td>";
+		for (var key in array) {
+			// do something with key
+			tbody += "<td>"+array[key]+"</td>";
+		}
+		tbody += "</tr>"
+		$(updated_table_body).append(tbody);
+		$(table_header).append(updated_table_body);
+	}
+
+	//######################
+	//##                  ##
+	//##    Drag stuff    ##
+	//##                  ##
+	//######################
+
+	//Helper function to keep table row from collapsing when being sorted
+	var fixHelperModified = function(e, tr) {
+	    var $originals = tr.children();
+	    var $helper = tr.clone();
+	    $helper.children().each(function(index) 
+	    {
+	      $(this).width($originals.eq(index).width())
+	    });
+	    return $helper;
+	};
+
+	//Make diagnosis table sortable
+	$("#diagnosis_list tbody").sortable({
+	    helper: fixHelperModified,
+	    stop: function(event,ui) {renumber_table('#diagnosis_list')}
+	}).disableSelection();
+
+	//Delete button in table rows
+	$('table').on('click','.btn-delete',function() {
+	    tableID = '#' + $(this).closest('table').attr('id');
+	    r = confirm('Delete this item?');
+	    if(r) {
+	        $(this).closest('tr').remove();
+	        renumber_table(tableID);
+	        }
+	});
+
 }
 
 $(document).on("click", ".pure-checkbox", function(){
-	var element = document.getElementById(this.id);
-	console.log(element);
+	var last_child = $(this).parent().parent().children(':last').children();
+	if ( $(this).is(':checked') ) {
+	    $(last_child).removeAttr('disabled')
+	} else {
+		$(last_child).removeAttr('style');
+		$(last_child).val('');
+		$(last_child).attr('disabled','');
+	}
 });
 
 
 
-
-
+//Delete button in table rows
+$('table').on('click','.btn-delete',function() {
+    tableID = '#' + $(this).closest('table').attr('id');
+    r = confirm('Delete this item?');
+    if(r) {
+        $(this).closest('tr').remove();
+        renumber_table(tableID);
+        }
+});
 
 
 
